1858452bcea6801e5a55c7d564c3b216931e01bd
Lider grupo 2700 <sisdesarrollo3@ucm.edu.co>
2022-10-16 12:00:20 -0500

    ejemplo web blazor

---
+++ CSHARP/02. FRONT-END-HTML-CS-JS/front-end/js/funciones.js
@@ -0,0 +1,12 @@
+function operaciones (){  
+    var numero1 = 10;
+    let numero2 = 20;
+    var resultado = numero1 + numero2;  
+    alert("RESULTADO: " + resultado);
+    edad = prompt("ingrese edad: ");
+    console.log("estoy en la funicon JS - por consola");
+    if (edad >= 18) mensaje = "ES APTO PARA VOTAR";
+    else mensaje = "NO ES APTO PARA VOTAR";
+    alert("SU EDAD ES: " + edad + " " + mensaje);
+
+ }
